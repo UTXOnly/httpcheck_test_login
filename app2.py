9706cd7bd18cd819b3d9c5acf8704d759a324e76
UTXOnly <brian.hartford@datadoghq.com>
2023-04-03 11:05:49 -0400

    Initial commit working

---
+++ app2.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, make_response
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+
+# Mock user database
+users = {'user1': 'password1', 'user2': 'password2'}
+
+@app.route('/')
+def home():
+    return "Welcome to the homepage!"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def do_login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+        # Set a cookie to mark the user as authenticated
+        resp = make_response(redirect('/protected'))
+        resp.set_cookie('authenticated', 'true')
+        return resp
+    else:
+        return "Invalid login credentials"
+
+@app.route('/logout')
+def logout():
+    # Clear the authentication cookie or session variable
+    resp = make_response(render_template('login.html'))
+    resp.delete_cookie('authenticated')
+    return resp
+
+@app.route('/protected')
+def protected():
+    # Check for the presence of the authentication cookie or session variable
+    authenticated = request.cookies.get('authenticated')
+    if authenticated != 'true':
+        return redirect('/login')
+    else:
+        return "Welcome to the protected page!"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
---
+++ test_commands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -X GET -c cookies.txt -b cookies.txt -d 'username=user1' -d 'password=password1' http://127.0.0.1:8000/login
+
+#This curl command sends a POST request to the /login endpoint with the username and password included in the request body as form data. The response from the server includes a cookie that marks the user as authenticated.
+#
+#To test the /protected endpoint, you can use the cookies saved in the cookies.txt file to authenticate the request:
+#
+curl -X GET -c cookies.txt -b cookies.txt http://127.0.0.1:8000/protected
+
+#This curl command sends a GET request to the /protected endpoint with the cookies saved in cookies.txt included in the request headers. If the cookies are valid and mark the user as authenticated, the server will respond with the protected content. Otherwise, it will redirect the user to the login page.
